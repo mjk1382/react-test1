c97af0dcfb94e08f975dce6ee916e4a51458a213
javad <mohammadjavadkashkooli@gmail.com>
2025-03-12 14:43:14 +0330

    part 1: project timer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+const tick=()=>{
+  const elem=(
+    <div>
+    <h1>hi guys</h1>
+    <h2> it is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+ ReactDOM.createRoot(document.getElementById("root")).render(elem);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+}
+setInterval(()=>{
+  tick()
+},1000)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// const set=()=>{
+//   const time=(<div>
+//     <h1>salammmmmm barikalllllllllllll</h1>
+//     <h2>{new Date().toLocaleTimeString()}</h2>
+//   </div>)
+//   const root=ReactDOM.createRoot(document.getElementById('root'))
+//   root.render(time)
+// }
+// setInterval(set,1000)
+
+
