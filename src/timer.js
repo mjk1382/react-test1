b99ba63114e811e429599c4aa18a5bdec55337f2
javad <mohammadjavadkashkooli@gmail.com>
2025-03-13 14:26:07 +0330

    takmil timer ba stop va start commit badi project timer

--- src/timer.js
+++ src/timer.js
@@ -10,24 +10,32 @@ class Timer extends React.Component{
           time : 100
         }
       }
-    
-      componentDidMount(){
+    startInterval=()=>{
         interval = setInterval(()=>{
-          this.setState({
-            time: this.state.time - 1
-          })
-        } , 1000)
+            this.setState({
+              time: this.state.time - 1
+            })
+          } , 1000)
+    }
+    
+    stopInterval=()=>{
+        clearInterval(interval)
+    }
+      componentDidMount(){
+        this.startInterval()
       }
       componentDidUpdate(){
         if(this.state.time==0){
-            clearInterval(interval)
-        }
+     this.stopInterval()
+      }
       }
       render(){
         
         return(<>
           <h2 className="timer">{this.state.time}</h2>
           <button onClick={this.props.handelText}>change</button>
+          <button onClick={this.startInterval}>start</button>
+          <button onClick={this.stopInterval}>stop</button>
           </>
         )
       }
